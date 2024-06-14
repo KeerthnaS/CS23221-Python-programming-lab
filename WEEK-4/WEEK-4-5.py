d8f64a1d36d40d97209515070fb6a84de501b7a2
KeerthnaS <231801083@rajalakshmi.edu.in>
2024-06-14 16:00:37 +0530

    Create WEEK-4-5.py

---
+++ WEEK-4/WEEK-4-5.py
@@ -0,0 +1,24 @@
+Non Repeated Digit Count:
+
+Write a program to find the count of non-repeated digits in a given number N. The number will be passed to the program as an input of type int.
+Assumption: The input number will be a positive integer number >= 1 and <= 25000.
+Some examples are as below.
+If the given number is 292, the program should return 1 because there is only 1 non-repeated digit '9' in this number
+If the given number is 1015, the program should return 2 because there are 2 non-repeated digits in this number, '0', and '5'.
+If the given number is 108, the program should return 3 because there are 3 non-repeated digits in this number, '1', '0', and '8'.
+If the given number is 22, the function should return 0 because there are NO non-repeated digits in this number.
+
+For example:
+Input	Result
+292	1
+1015	2
+108	3
+22	0
+
+Program:
+
+a={}
+for i in input:
+   if i in a:a[i]+=1
+   else:a[i]=1
+print(sum([1 for i in a if a[i]==1]))
