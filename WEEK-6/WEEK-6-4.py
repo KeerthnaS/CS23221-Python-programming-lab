21846b3b90bfd655c283c494e1a13c7cf68409ad
KeerthnaS <231801083@rajalakshmi.edu.in>
2024-06-14 16:13:12 +0530

    Create WEEK-6-4.py

---
+++ WEEK-6/WEEK-6-4.py
@@ -0,0 +1,53 @@
+Distinct Elements in an Array:
+
+Program to print all the distinct elements in an array. Distinct elements are nothing but the unique (non-duplicate) elements present in the given array.
+Input Format:
+First line take an Integer input from stdin which is array length n.
+Second line take n Integers which is inputs of array.
+Output Format:
+Print the Distinct Elements in Array in single line which is space Separated
+
+Example Input:
+5
+1 
+2 
+2 
+3 
+4
+Output:
+1 2 3 4
+Example Input:
+6
+1 
+1 
+2 
+2 
+3 
+3
+Output:
+1 2 3
+
+For example:
+Input	Result
+5
+1 
+2 
+2 
+3
+4	1 2 3 4 
+6
+1 
+1 
+2 
+2 
+3 
+3	1 2 3 
+
+Program:
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+distinct_elements = set(arr)
+print(*distinct_elements)
